eb037b7d7686741cdb1906d8b0aecf1e057f29c5
Arpita Gaur <126095597+Arpita-Gaur@users.noreply.github.com>
2024-07-09 13:47:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function Board() {
   return (
     <div>
       <h1 class="Color">TIC-TAC-TOE</h1>
-      <div className="status">{status}</div>
+      <div class="Color" className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
